b6082be75ded3eb8d98632c9bf1a721690d48a9b
poro777 <55618414+poro777@users.noreply.github.com>
2025-08-01 15:17:37 +0800

    ui: display images, add folder, remove folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,219 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <BButton variant="primary" @click="addFolder">Add Folder</BButton>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="directory-block">
+    <BInputGroup
+      prepend="Query"
+      class="mt-3"
+    >
+      <BFormInput v-model="queryText" @keyup.enter="fetchResults"/>
+      <BButton variant="info" @click="fetchResults">Go</BButton>
+    </BInputGroup>
+
+    <div v-if="results.length" class="gallery" >
+      <div v-for="(img, index) in results" :key="index" class="thumbnail" @click="openModal(img.full_path)">
+        <img :src="getImageUrl(img.full_path)" />
+      </div>
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div
+    v-for="(group, dirname) in groupedImages"
+    :key="dirname"
+    class="directory-block"
+  >
+    <h2 class="directory-title">📁 Folder {{ dirname }}</h2>
+    <BButton variant="primary" @click="deleteFolder(dirname)">Delete Folder</BButton>
+    <div class="gallery">
+      <div
+        v-for="(img, index) in group"
+        :key="img.id"
+        class="thumbnail"
+        @click="openModal(img.full_path)"
+      >
+        <!-- User full_path for dev-->
+        <img :src="getImageUrl(img.full_path)" />
+      </div>
+    </div>
+  </div>
+
+  <div v-if="showModal" class="modal" @click.self="closeModal">
+
+    <img :src="getImageUrl(currentImage)" class="modal-image" />
+    <BButton variant="primary" @click="openInfo" class="info-btn" size="sm" pill>!</BButton>
+    <BButton variant="primary" @click="closeModal" class="close-btn" size="sm" pill>x</BButton>
+    
+    <BModal v-model="showInfo" title="Info" style="z-index:101"> 
+      <p>Image Path: {{ currentImage }}</p>
+    </BModal>
+    
+  </div>
 </template>
 
+
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import axios from 'axios'
+
+const images = ref([])
+const showModal = ref(false)
+const showInfo = ref(false)
+const currentGroup = ref([])
+const currentIndex = ref(0)
+const show = ref(false)
+const queryText = ref('')
+const results = ref([])
+const currentImage = ref('')
+
+
+const getImageUrl = (path) => `http://127.0.0.1:8000/file?path=${encodeURIComponent(path)}`
+
+// Group by directory_id
+const groupedImages = computed(() => {
+  const groups = {} 
+  for (const img of images.value) {
+    const full_path = img.full_path
+    const dirPath = full_path.substring(0, full_path.lastIndexOf('/'))
+    if (!groups[dirPath]) {
+      groups[dirPath] = []
+    }
+    groups[dirPath].push(img)
+  }
+  return groups
+})
+
+
+const openModal = (full_path) => {
+  currentImage.value = full_path
+  showModal.value = true
+}
+
+const openInfo = () => {
+  showInfo.value = true
+}
+const closeModal = () => {
+  showModal.value = false
+  showInfo.value = false
+}
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://127.0.0.1:8000/image')
+    images.value = res.data
+  } catch (err) {
+    console.error('Failed to load images', err)
+  }
+})
+
+const addFolder = async () => {
+  const folderPath = await window.electronAPI.selectFolder()
+  if (!folderPath) return
+
+  await axios.post('http://127.0.0.1:8000/watcher/add', null, {
+    params: { path: folderPath }
+  })
+  alert(`Folder added: ${folderPath}`)
+}
+
+
+const deleteFolder = async (folderPath) => {
+
+  await axios.delete('http://127.0.0.1:8000/watcher/remove', {
+    params: { path: folderPath , delete_images: true}
+  })
+  alert(`Folder removed: ${folderPath}`)
+}
+
+const fetchResults = async () => {
+  try {
+    const res = await axios.get('http://127.0.0.1:8000/api/query', {
+      params: { text: queryText.value, use_text_embed : true, use_bm25 : true, use_joint_embed : true }
+    })
+    results.value = res.data
+  } catch (err) {
+    console.error('Search failed', err)
+    alert('Search failed. Check console for details.')
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.directory-group {
+  margin-bottom: 3rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.thumbnail img {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 1px solid #ccc;
 }
+
+
+.modal {
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.85);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 100%;
+  z-index: 100;
+}
+.modal-image {
+  max-width: 90%;
+  max-height: 90%;
+  border-radius: 8px;
+}
+
+.close-btn {
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+
+.info-btn {
+  position: absolute;
+  top: 20px;
+  right: 80px;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+
+
+.directory-block {
+  border: 2px solid #ddd;
+  border-radius: 8px;
+  padding: 1rem;
+  margin-bottom: 2rem;
+  background-color: #f9f9f9;
+}
+
+.directory-title {
+  font-size: 1.2rem;
+  margin-bottom: 0.75rem;
+  color: #333;
+  font-weight: bold;
+  word-break: break-all;
+}
+
+
 </style>
